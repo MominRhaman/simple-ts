a17969638ebfbec31e02e375ea7e28121520a5bd
MominRhaman <mominrhaman007@gmail.com>
2024-08-20 16:08:51 +0600

    first commit

---
+++ src/alises.ts
@@ -0,0 +1,14 @@
+type stringNum = string | number;
+type userType = { name: string; age: number };
+
+
+const userDetails = (
+    id: stringNum,
+    user: userType
+) => {
+    console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+
+const sayHello = (user: userType) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
